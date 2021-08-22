381756a15cd34791e831047b59c1ae1646dd8c03
Alvaro Aguirre <alvaro1337@hotmail.com>
2021-08-21 23:45:01 -0300

    Add rule

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useCallback } from "react";
+import React, { FunctionComponent } from "react";
 import { useRequest } from "./client/useRequest";
 
 type UsersResponse = {

86144060e2df3cf0e807afb6e427095cadc99189
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2022-06-24 16:55:30 -0300

    Clean code

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,5 @@
 import React, { FunctionComponent } from "react";
-import { loginUser, getUsers } from "./client/endpoints";
-import { useRequest } from "@/client";
-
-type UsersResponse = {
-  avatar: string;
-  createdAt: string;
-  email: string;
-  level: number;
-  name: string;
-  password: string;
-  points: number;
-  surname: string;
-}[];
-
-type LoginResponseType = {
-  success: string;
-  token: string;
-  id: string;
-  email: string;
-  name: string;
-  surname: string;
-  avatar: string;
-};
 
 export const App: FunctionComponent = () => {
-  const body = {
-    email: "alva@gmail.com",
-    password: "1234",
-  };
-
-  //const { data, isFetching, error, dispatch, clearErrors } = useRequest<LoginResponseType>(loginUser(body));
-
-  const { data, isFetching, error, dispatch, clearErrors, disableInterval } = useRequest<UsersResponse>(
-    getUsers(),
-    { onReceive: () => console.log("On receive") }
-  );
-
-  return (
-    <>
-      <button onClick={dispatch}>Fetch</button>
-      <button onClick={clearErrors}>Clear errors</button>
-      <>
-        {isFetching && "fetching"}
-        {error && JSON.stringify(error)}
-        {data && JSON.stringify(data)}
-      </>
-    </>
-  );
+  return <div>app</div>;
 };
